33b372d06c0c4db4b6b535a5c5ff06a8b2d1c75c
Joe (Yu) Zhou <yzhou61@gmail.com>
2019-01-04 20:34:46 -0600

    my improvements

--- src/visodo.cpp
+++ src/visodo.cpp
@@ -34,6 +34,29 @@ using namespace std;
 
 // IMP: Change the file directories (4 places) according to where your dataset is saved before running!
 
+static cv::Vec3f rotationMatrixToEulerAngles(cv::Mat &R)
+{
+    float sy = sqrt(R.at<double>(0,0) * R.at<double>(0,0) +  R.at<double>(1,0) * R.at<double>(1,0) );
+
+    bool singular = sy < 1e-6; // If
+
+    float x, y, z;
+    if (!singular)
+    {
+        x = atan2(R.at<double>(2,1) , R.at<double>(2,2));
+        y = atan2(-R.at<double>(2,0), sy);
+        z = atan2(R.at<double>(1,0), R.at<double>(0,0));
+    }
+    else
+    {
+        x = atan2(-R.at<double>(1,2), R.at<double>(1,1));
+        y = atan2(-R.at<double>(2,0), sy);
+        z = 0;
+    }
+
+    return cv::Vec3f(x, y, z);
+}
+
 double getAbsoluteScale(int frame_id, int sequence_id, double z_cal)	{
   
   string line;
@@ -79,11 +102,17 @@ int main( int argc, char** argv )	{
   ofstream myfile;
   myfile.open ("results1_1.txt");
 
+  const char *dir = "/extra/SLAM/00/image_0";
+  int starting = 1;
+  double sensor_height = 6.2743998;
+  double sensor_width = 4.7181997;
+  double sensor_focal = 4.67;
+
   double scale = 1.00;
   char filename1[200];
   char filename2[200];
-  sprintf(filename1, "/home/avisingh/Datasets/KITTI_VO/00/image_2/%06d.png", 0);
-  sprintf(filename2, "/home/avisingh/Datasets/KITTI_VO/00/image_2/%06d.png", 1);
+  sprintf(filename1, "%s/%06d.png", dir, starting++);
+  sprintf(filename2, "%s/%06d.png", dir, starting++);
 
   char text[100];
   int fontFace = FONT_HERSHEY_PLAIN;
@@ -112,6 +141,7 @@ int main( int argc, char** argv )	{
   //TODO: add a fucntion to load these values directly from KITTI's calib files
   // WARNING: different sequences in the KITTI VO dataset have different intrinsic/extrinsic parameters
   double focal = 718.8560;
+  //cv::Point2d pp(540, 960);
   cv::Point2d pp(607.1928, 185.2157);
   //recovering the pose and the essential matrix
   Mat E, R, t, mask;
@@ -135,8 +165,8 @@ int main( int argc, char** argv )	{
 
   Mat traj = Mat::zeros(600, 600, CV_8UC3);
 
-  for(int numFrame=2; numFrame < MAX_FRAME; numFrame++)	{
-  	sprintf(filename, "/home/avisingh/Datasets/KITTI_VO/00/image_2/%06d.png", numFrame);
+  for(int numFrame=starting; numFrame < MAX_FRAME; numFrame++)	{
+  	sprintf(filename, "%s/%06d.png", dir, numFrame);
     //cout << numFrame << endl;
   	Mat currImage_c = imread(filename);
   	cvtColor(currImage_c, currImage, COLOR_BGR2GRAY);
@@ -157,7 +187,8 @@ int main( int argc, char** argv )	{
   		currPts.at<double>(1,i) = currFeatures.at(i).y;
     }
 
-  	scale = getAbsoluteScale(numFrame, 0, t.at<double>(2));
+  	//scale = getAbsoluteScale(numFrame, 0, t.at<double>(2));
+    scale = 1;
 
     //cout << "Scale is " << scale << endl;
 
@@ -165,6 +196,10 @@ int main( int argc, char** argv )	{
 
       t_f = t_f + scale*(R_f*t);
       R_f = R*R_f;
+
+      Vec3f rr = rotationMatrixToEulerAngles(R);
+
+      cout << rr * 180 / CV_PI << '\n';
 
     }
   	
@@ -184,6 +219,12 @@ int main( int argc, char** argv )	{
 
  	  }
 
+    for (int i = 0; i < currFeatures.size(); ++i) {
+        circle(currImage_c, prevFeatures[i], 4, CV_RGB(255, 0, 0));
+        circle(currImage_c, currFeatures[i], 4, CV_RGB(0, 0, 255));
+        line(currImage_c, prevFeatures[i], currFeatures[i], CV_RGB(0, 255, 0));
+    }
+
     prevImage = currImage.clone();
     prevFeatures = currFeatures;
 
@@ -198,7 +239,7 @@ int main( int argc, char** argv )	{
     imshow( "Road facing camera", currImage_c );
     imshow( "Trajectory", traj );
 
-    waitKey(1);
+    waitKey();
 
   }
 
